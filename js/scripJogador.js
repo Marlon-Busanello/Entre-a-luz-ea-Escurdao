9851baeb9064de29056fb95baee66db6e8f4bd18
mateco <143560811+mateco@users.noreply.github.com>
2023-11-02 20:06:57 -0300

    rolagem de dados

--- js/scripJogador.js
+++ js/scripJogador.js
@@ -34,3 +34,30 @@ document.getElementById("botaoEditar").addEventListener("click", toggleEdicao);
 
 // Carregar os valores ao carregar a página
 carregarValores();
+
+// Função para gerar um valor aleatório entre 1 e um número específico
+function rolarDado(maxValue, resultElement) {
+    const resultado = Math.floor(Math.random() * maxValue) + 1;
+    resultElement.textContent = resultado;
+  }
+  
+  // Adiciona ouvintes de evento a todos os botões
+  const dados = [
+    { id: "rolarD20", maxValue: 20 },
+    { id: "rolarD12", maxValue: 12 },
+    { id: "rolarD10", maxValue: 10 },
+    { id: "rolarD8", maxValue: 8 },
+    { id: "rolarD6", maxValue: 6 },
+    { id: "rolarD4", maxValue: 4 },
+    { id: "rolarD100", maxValue: 100 }
+  ];
+  
+  dados.forEach(dado => {
+    const botao = document.getElementById(dado.id);
+    const resultDice = document.querySelector("#resultRolagem");
+  
+    botao.addEventListener("click", function() {
+      rolarDado(dado.maxValue, resultDice);
+    });
+  });
+  
